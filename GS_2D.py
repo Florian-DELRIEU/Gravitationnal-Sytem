582cf3cfcc99733d5b8105cb9961d409b1effa61
Florian Delrieu <florian.delrieu@free.fr>
2020-06-20 20:00:31 +0200

    [Update] v0.3.4 - First Plot

--- GS_2D.py
+++ GS_2D.py
@@ -7,17 +7,15 @@ import matplotlib.pyplot as plt
 # Maillage
 dx, x_range = 0.1, 10
 dy, y_range = 0.1, 10
-dt, tf = 0.1, 10
 X,Y = np.meshgrid(
     np.arange(-x_range,x_range,dx),
     np.arange(-y_range,y_range,dy))
-t = np.arange(0,tf,dt)
-Nt, Nx, Ny = len(t), len(X), len(Y)
 G = 1 # Constante Grav (6.7e8)
 Pos = [
     (0,0), # x,y
 ]
 Mass = [1]
-Pot = lambda x,y: -G*Mass[0] / ( np.sqrt((X-Pos[0][0])**2+(Y-Pos[0][1])**2) )**3
-
-plt.contourf(X,Y,Pot(X,Y))
+Potential = lambda x,y: G*Mass[0] / ( np.sqrt((x-Pos[0][0])**2+(y-Pos[0][1])**2) )**3
+POTENT = Potential(X,Y)
+POTENT_contour = np.linspace(POTENT.min(),POTENT.max(),10)
+plt.contourf(X,Y,POTENT,[0,.1,2,5,20])
