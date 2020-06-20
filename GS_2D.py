46a01737bf0945865919aaa9451a6bb84d973a4a
Florian Delrieu <florian.delrieu@free.fr>
2020-06-20 17:08:54 +0200

    [Minor Update] v0.3 - Add Maillage in master

--- GS_2D.py
+++ GS_2D.py
@@ -1,3 +1,14 @@
 """
 Programme simulant un sytème gravitationnel a N corps en 2D
 """
+import numpy as np
+
+# Maillage
+dx, x_range = 0.1, 10
+dy, y_range = 0.1, 10
+dt, tf = 0.1, 10
+X,Y = np.meshgrid(
+    np.arange(-x_range,x_range,dx),
+    np.arange(-y_range,y_range,dy))
+t = np.arange(0,tf,dt)
+Nt, Nx, Ny = len(t), len(X), len(Y)
