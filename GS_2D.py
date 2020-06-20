7b53f3d2d068b9b5d4a2210f769046b941183f60
Florian Delrieu <florian.delrieu@free.fr>
2020-06-20 16:47:29 +0200

    [Minor Update] v0.2.1 - Astral body class

--- GS_2D.py
+++ GS_2D.py
@@ -1,3 +1,10 @@
 """
 Programme simulant un sytème gravitationnel a N corps en 2D
 """
+
+class AstralObject:
+    def __init__(self):
+        self.Mass = 1
+        self.Vx = 0
+        self.Vy = 0
+        self.IsMoving = True
