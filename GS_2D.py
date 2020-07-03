3c742f7356e41a000ebc8f170ced2d0f01c59a12
Florian Delrieu <florian.delrieu@free.fr>
2020-07-03 21:48:30 +0200

    [Update] v0.2.4 - Improve AstralObject() class

--- GS_2D.py
+++ GS_2D.py
@@ -2,15 +2,31 @@
 Programme simulant un sytème gravitationnel a N corps en 2D
 """
 import numpy as np
+import matplotlib.pyplot as plt
 
 class AstralObject:
     def __init__(self):
-        self.Mass = 1
-        self.x = 0
-        self.y = 0
-        self.Vx = 0
-        self.Vy = 0
+        self.Mass = int()
+        self.x = int()
+        self.y = int()
+        self.Vx = int()
+        self.Vy = int()
+        self.Distance = int()
+        self.Potential = int()
         self.IsMoving = True
+        self.setMass(1)
+        self.setPos(0,0)
+    def getDistance(self):  return np.sqrt((X-self.x)**2 + (Y-self.y)**2)
+    def getPotential(self): return - G*self.Mass/self.Distance**3
+    def setPos(self,x,y):
+        self.x = x
+        self.y = y
+        self.Distance = self.getDistance
+        self.Potential = self.getPotential
+    def setMass(self,m):
+        self.Mass = m
+
+G = 1
 
 # Maillage
 dx, x_range = 0.1, 10
@@ -21,3 +37,10 @@ X,Y = np.meshgrid(
     np.arange(-y_range,y_range,dy))
 t = np.arange(0,tf,dt)
 Nt, Nx, Ny = len(t), len(X), len(Y)
+
+A1 = AstralObject()
+A1.setPos(3,7)
+plt.figure(1)
+plt.contourf(X,Y,np.log(abs(A1.Potential)))
+plt.plot(A1.x,A1.y,"r*")
+plt.show()
