9ae8d6767dbc0e2173a38207923d6555017ae6a2
Florian Delrieu <florian.delrieu@free.fr>
2020-07-04 02:02:27 +0200

    [Update] v0.2.10

--- GS_2D.py
+++ GS_2D.py
@@ -13,38 +13,35 @@ class AstralObject:
         self.y = int()
         self.Vx = int()
         self.Vy = int()
-        self.Dist_x = np.array(0)
-        self.Dist_y = np.array(0)
+        self.Ax = int()
+        self.Ay = int()
+        self.IsMoving = True
         self.Distance = np.array(0)
         self.Grav_x = np.array(0)
         self.Grav_y = np.array(0)
-        self.Gravity = np.array(0)
-        self.IsMoving = True
         self.setPos(0,0)
         self.setMass(1)
     def getDistance(self):
-        self.Dist_x = np.array( X - self.x )
-        self.Dist_y = np.array( Y - self.y )
-        self.Distance = np.sqrt(self.Dist_x**2 + self.Dist_y**2)
+        self.Distance = np.sqrt((X-self.x)**2 + (Y-self.y)**2)
         self.getGravity()
     def getGravity(self):
-        self.Grav_x = np.array(- G * self.Mass / self.Dist_x)
-        self.Grav_y = np.array(- G * self.Mass / self.Dist_y)
-        self.Gravity = np.sqrt(self.Grav_x**2 + self.Grav_y**2)
+        self.Grav_x = -G*self.Mass/self.Distance**3 * (X-self.x)
+        self.Grav_y = -G*self.Mass/self.Distance**3 * (Y-self.y)
     def setPos(self,x,y):
         self.x = x
         self.y = y
         self.getDistance()
     def setMass(self,m):
         self.Mass = m
+        self.getGravity()
 
 # Global Parametres
 G = 1  # Constante Gravitationnelle
 Body = list()
 
 # Maillage
-dx, x_range = 0.1, 10
-dy, y_range = 0.1, 10
+dx, x_range = 1, 10
+dy, y_range = 1, 10
 dt, tf = 0.1, 10
 X,Y = np.meshgrid(
     np.arange(-x_range,x_range,dx),
@@ -56,12 +53,12 @@ Nt, Nx, Ny = len(t), len(X), len(Y)
 # Testing
 for _ in np.arange(2): Body.append(AstralObject())
 Body[0].setPos(0,0)
+Body[0].setMass(1)
 Body[1].setPos(1,0)
+Body[1].setMass(0)
 
-GRAVITY = getGRAVITY(Body)
 plt.figure(1)
-Gravity_level = np.logspace(Body[0].Gravity.min(),Body[0].Gravity.max(),10)
-plt.contourf(X,Y,Body[0].Gravity)
-plt.plot(Body[0].x,Body[0].y,"r*")
-plt.plot(Body[1].x,Body[1].y,"r*")
+plt.quiver(X,Y,Body[0].Grav_x,Body[0].Grav_y)
+#plt.plot(Body[0].x,Body[0].y,"r*")
+#plt.plot(Body[1].x,Body[1].y,"r*")
 plt.show()
