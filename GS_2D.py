9ac8e841ee22b22d0bdb3c47427eb211d03149ee
Florian Delrieu <florian.delrieu@free.fr>
2020-07-04 02:49:34 +0200

    [Update] v0.2.11 - Quiver and mask

--- GS_2D.py
+++ GS_2D.py
@@ -2,6 +2,7 @@
 Programme simulant un sytème gravitationnel a N corps en 2D
 """
 import numpy as np
+import numpy.ma as ma  # masque
 import matplotlib.pyplot as plt
 
 def getGRAVITY(body_list): return sum(_.Gravity for _ in body_list)
@@ -22,7 +23,7 @@ class AstralObject:
         self.setPos(0,0)
         self.setMass(1)
     def getDistance(self):
-        self.Distance = np.sqrt((X-self.x)**2 + (Y-self.y)**2)
+        self.Distance = ma.masked_equal(np.sqrt((X-self.x)**2 + (Y-self.y)**2),0)
         self.getGravity()
     def getGravity(self):
         self.Grav_x = -G*self.Mass/self.Distance**3 * (X-self.x)
@@ -34,7 +35,6 @@ class AstralObject:
     def setMass(self,m):
         self.Mass = m
         self.getGravity()
-
 # Global Parametres
 G = 1  # Constante Gravitationnelle
 Body = list()
@@ -54,11 +54,16 @@ Nt, Nx, Ny = len(t), len(X), len(Y)
 for _ in np.arange(2): Body.append(AstralObject())
 Body[0].setPos(0,0)
 Body[0].setMass(1)
+Body[0].IsMoving = False
 Body[1].setPos(1,0)
 Body[1].setMass(0)
 
+# Plotting
+msk_Grav_x = ma.masked_outside(Body[0].Grav_x,0.08,-0.08)  # Masque les valeurs hors des limites
+msk_Grav_y = ma.masked_outside(Body[0].Grav_y,0.08,-0.08)
 plt.figure(1)
-plt.quiver(X,Y,Body[0].Grav_x,Body[0].Grav_y)
+#plt.quiver(X,Y,Body[0].Grav_x,Body[0].Grav_y)
+plt.quiver(X,Y,msk_Grav_x,msk_Grav_y)
 #plt.plot(Body[0].x,Body[0].y,"r*")
 #plt.plot(Body[1].x,Body[1].y,"r*")
 plt.show()
