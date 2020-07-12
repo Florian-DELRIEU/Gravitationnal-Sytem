4c7db8c788dbbc27b6c8f45462c3f3132c71ee49
Florian Delrieu <florian.delrieu@free.fr>
2020-07-12 10:52:06 +0200

    [Update] v0.3.6

--- GS_2D.py
+++ GS_2D.py
@@ -10,16 +10,17 @@ dy, y_range = 0.1, 10
 X,Y = np.meshgrid(
     np.arange(-x_range,x_range,dx),
     np.arange(-y_range,y_range,dy))
+
+# Parametres et corps
 G = 1 # Constante Grav (6.7e8)
 Pos = [
     (0,0), # x,y
-    (1,0),
-]
+    (1,0),]
 Mass = [10,1]
-Gravity_func = lambda x,y: G*Mass[0] / ( np.sqrt((x-Pos[0][0])**2+(y-Pos[0][1])**2) )**2
-GRAVITY = Gravity_func(X,Y)
-GRAVITY_contourlevel = np.logspace(GRAVITY.min(),GRAVITY.max(),30) # [0,.1,2,5,20]
-plt.contourf(X,Y,GRAVITY,GRAVITY_contourlevel)
+
+Gx_func = lambda x, y:G*Mass[0]/abs(x - Pos[0][0])**3 * (x - Pos[0][0])
+Gy_func = lambda x, y:G*Mass[0]/abs(y - Pos[0][1])**3 * (y - Pos[0][1])
+
 plt.plot(Pos[0][0],Pos[0][1],"r*")
 plt.plot(Pos[1][0],Pos[1][1],"r*")
 plt.show()
