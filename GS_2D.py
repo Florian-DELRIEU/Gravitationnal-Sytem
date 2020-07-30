0ddd2b3b4b9692d7d0ed00d91b6d652ca420a41e
Florian Delrieu <florian.delrieu@free.fr>
2020-07-30 19:13:16 +0200

    [v0.2b.5] Update - Now working

--- GS_2D.py
+++ GS_2D.py
@@ -9,7 +9,7 @@ def GRAVITYFIELD(body_list): pass
 class AstralBody:
     def __init__(self):
     # Variable definitions
-        self.Mass = float()
+        self.Mass = float(0)
         self.x = float(0)
         self.y = float(0)
         self.vx = float(0)
@@ -19,16 +19,18 @@ class AstralBody:
         self.Body_list = list()
         self.IsMoving = True
     def refresh(self,dt):
-        self.Body_list = Body.copy() # Body_list.remove(self) ne marche plus après ...
-        self.Body_list.remove(self)
-        self.ax, self.ay = 0,0
-        for this_body in self.Body_list:
-            self.ax += this_body.x
-            self.ay += this_body.y
-            self.vx += self.ax*dt
-            self.vy += self.ay*dt
-            self.x += self.vx*dt
-            self.y += self.vy*dt
+        if self.IsMoving:
+            self.Body_list = Body.copy() # Body_list.remove(self) ne marche plus après ...
+            self.Body_list.remove(self)
+            self.ax, self.ay = 0,0
+            for this_body in self.Body_list:
+                cur_distance = np.sqrt((this_body.x - self.x)**2 + (this_body.y - self.y)**2)
+                self.ax += - G * this_body.Mass / cur_distance**3 * (self.x - this_body.x)
+                self.ay += - G * this_body.Mass / cur_distance**3 * (self.y - this_body.y)
+                self.vx += self.ax*dt
+                self.vy += self.ay*dt
+                self.x += self.vx*dt
+                self.y += self.vy*dt
     def __repr__(self):
         txt = """Astral Body
             - Pos = ({} , {})
@@ -53,18 +55,23 @@ t = np.arange(0,tf,dt)
 Nt, Nx, Ny = len(t), len(X), len(Y)
 
 
-# Testing
+# Creating body
 for _ in np.arange(2):Body.append(AstralBody())  # Ajout des corps celestes
 a = Body[0]
 b = Body[1]
-for thisbody in Body: thisbody.refresh(0.1)
-
-Body[1].x,Body[1].y = 1,0
-
-# Simulation
-
+a.Mass,b.Mass = 1,1
+a.IsMoving = False
+b.x,b.y = 1,0
+b.vx,b.vy = 0,1
 
 plt.figure(1)
-plt.plot(Body[0].x,Body[0].y,"r*")
-plt.plot(Body[1].x,Body[1].y,"r*")
+#plt.clf()
+for this_body in Body:
+    plt.plot(this_body.x,this_body.y,"r*")
 plt.show()
+
+# Simulation
+for _ in t:
+    for this_body in Body: this_body.refresh(dt)
+    plt.pause(dt)
+    plt.plot(b.x,b.y,'b+')
