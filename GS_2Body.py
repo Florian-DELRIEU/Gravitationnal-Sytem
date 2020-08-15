40f6029818febb551c3ab9d5f2002fab49327095
Florian Delrieu <florian.delrieu@free.fr>
2020-08-15 12:15:51 +0200

    [v2.2.2] Update

--- GS_2Body.py
+++ GS_2Body.py
@@ -20,16 +20,27 @@ b.setvelocity(0,1)
 
 # Simulation
 plt.figure("Trajectory")
+Bacc = np.array([])
+Bdist = np.array([])
 for _ in D.t:
     plt.pause(0.1)
     plt.clf()
     for this_body in Body:
+        plt.arrow(b.x,b.y,b.ax,b.ay)
         this_body.refresh(D.dt)
         plt.plot(this_body.x,this_body.y,this_body.Color+this_body.Mark)
         plt.title("{} / {}".format(_,D.tf))
         plt.xlim(-5,5)
         plt.ylim(-5,5)
+    Bacc = np.append(Bacc,np.sqrt(b.ax**2 + b.ay**2))
+    Bxy = np.append(Bxy,np.sqrt(b.x**2 + b.y**2))
 for this_body in Body:
     if this_body.IsMoving:
         this_body.Trajectory = np.array(this_body.Trajectory)
         plt.plot(this_body.Trajectory[:,0],this_body.Trajectory[:,1],this_body.Color+"-")
+
+plt.figure("Acc")
+plt.plot(D.t,Bacc,"b-")
+plt.figure("Dist")
+plt.plot(D.t,Bxy,"b-")
+plt.show()
