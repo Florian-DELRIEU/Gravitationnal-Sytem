ac123d7dbc70f75d39c590b4f2f7fecf4f28f59f
floriandelrieu <florian.delrieu@free.fr>
2021-06-04 14:43:29 +0200

    correcting windows and mac pathing

--- GS_2Body.py
+++ GS_2Body.py
@@ -9,19 +9,19 @@ import time as t
 start = t.time()
 # Global Parametres
 D = Domain()
-D.dt = 1e-2
+D.dt = 1e-3
 D.tf = 10
 D.settime(D.dt,D.tf)
 dt = D.dt
 
 # Creating bodies
 a = AstralBody(D)
-a.setbody(0,0,10)
-a.setvelocity(0,0)
+a.setbody(-0.1,0,10)
+a.setvelocity(0,np.sqrt(0.1))
 
 b = AstralBody(D)
-b.setbody(1,0,0.1)
-b.setvelocity(0,np.sqrt(10))
+b.setbody(0.9,0,1)
+b.setvelocity(0,np.sqrt(9))
 
 c = AstralBody(D)
 c.setbody(2,0,0.5)
@@ -31,8 +31,7 @@ c.setvelocity(0,np.sqrt(10/5))
 for _ in D.t:
     for this_body in D.BodyList: this_body.refresh(dt)
 
-
-path = "/Users/floriandelrieu/OneDrive/Cours/Python/Etudes/Gravitationnal-Sytem/Datas/"
+path = "Datas/"
 Dict2CSV(a.Kinetic,path+"a_Kinetic.csv")
 Dict2CSV(b.Kinetic,path+"b_Kinetic.csv")
 
