f0b5361a49645b52fe02476d810758ccbdc083cd
floriandelrieu <florian.delrieu@free.fr>
2021-06-04 18:24:57 +0200

    Rename file and create CircularOrbit_plot

--- GSplot.py
+++ GSplot.py
@@ -59,30 +59,3 @@ def NumericalRelativeSpeed(CSVfile,mark="",grid=True):
     y = np.array(Data["y"])
     plt.semilogy(t,abs(np.sqrt(vx**2+vy**2)*dt/np.sqrt(x**2+y**2)),mark)
     if grid: plt.grid("both")
-
-
-###
-path = "Datas/"
-plt.figure("Trajectory")
-PlotTrajectory(path+"a_Kinetic.csv")
-PlotTrajectory(path+"b1_Kinetic.csv")
-PlotTrajectory(path+"b2_Kinetic.csv")
-PlotTrajectory(path+"b3_Kinetic.csv")
-
-plt.figure("Speed")
-PlotSpeed(path+"a_Kinetic.csv")
-PlotSpeed(path+"b1_Kinetic.csv")
-PlotSpeed(path+"b2_Kinetic.csv")
-PlotSpeed(path+"b3_Kinetic.csv")
-
-plt.figure("Distance")
-PlotDistance(path+"a_Kinetic.csv")
-PlotDistance(path+"b1_Kinetic.csv")
-PlotDistance(path+"b2_Kinetic.csv")
-PlotDistance(path+"b3_Kinetic.csv")
-
-plt.figure("Numerical")
-NumericalRelativeSpeed(path+"a_Kinetic.csv")
-NumericalRelativeSpeed(path+"b1_Kinetic.csv")
-NumericalRelativeSpeed(path+"b2_Kinetic.csv")
-NumericalRelativeSpeed(path+"b3_Kinetic.csv")
