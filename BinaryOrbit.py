fbf17a9e8c55494ebefaed261f85876ffbcf3ab8
Florian Delrieu <florian.delrieu@free.fr>
2021-06-08 13:48:23 +0200

    Test OrbiteBinaire cas 1

--- BinaryOrbit.py
+++ BinaryOrbit.py
@@ -7,30 +7,35 @@ from MyPack.FFT import *
 from MyPack.Convert import *
 import time as t
 
-SIMULATION = True
+SIMULATION = False
 PLOTTING = True
 
 if SIMULATION:
     start = t.time()
     # Global Parametres
     D = Domain()
-    D.dt = 1e-3
-    D.tf = 30
+    D.dt = 1e-4
+    D.tf = 20
     D.settime(D.dt,D.tf)
     dt = D.dt
 
-    # Corps A
     a = AstralBody(D)
-    a.setbody(-0.1,0,10)
-    a.setvelocity(0,-np.sqrt(0.1))
-    # Corps B
     b = AstralBody(D)
-    b.setbody(0.9,0,1)
-    b.setvelocity(0,np.sqrt(9))
+    a.setbody(-1,0,10)
+    b.setbody(1,0,10)
+
+    a.setvelocity(0,np.sqrt(b.Mass)/2)
+    b.setvelocity(0,-np.sqrt(a.Mass)/2)
 
     # Simulation
-    for _ in D.t:
+    for i,_ in enumerate(D.t):
         for this_body in D.BodyList: this_body.refresh(dt)
+        if i == 0.01*len(D.t): print("1 %")
+        if i == 0.1*len(D.t): print("10 %")
+        if i == 0.3*len(D.t): print("30 %")
+        if i == 0.5*len(D.t): print("50 %")
+        if i == 0.7*len(D.t): print("70 %")
+        if i == 0.9*len(D.t): print("90 %")
 
     path = "Datas/"
     Dict2CSV(a.Kinetic,path+"a_Kinetic.csv")
@@ -41,7 +46,7 @@ if SIMULATION:
     print("Simulation time {}".format(duration))
 
 if PLOTTING:
-    path = "Datas/Orbite binaire stable dt1e-2/"
+    path = "Datas/OrbiteBinaire1_dt1e-2"
     plt.figure("Trajectory")
     PlotTrajectory(path + "a_Kinetic.csv")
     PlotTrajectory(path + "b_Kinetic.csv")
