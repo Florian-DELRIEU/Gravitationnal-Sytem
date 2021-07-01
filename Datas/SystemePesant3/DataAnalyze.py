4618237a6f3130d664bef36558bdff19ddc74381
Florian Delrieu <florian.delrieu@free.fr>
2021-07-01 22:30:12 +0200

    update systeme pesant 3
    - Data Analyze

--- Datas/SystemePesant3/DataAnalyze.py
+++ Datas/SystemePesant3/DataAnalyze.py
@@ -6,8 +6,8 @@ import MyPack.FFT as fft
 plt.ion()
 os.chdir("Datas/SystemePesant3/")
 
-Plot_PSD = True
-Specific_Plot = False
+Plot_psdA = True
+
 
 Files_list = [
     "A_Kinetic.csv",
@@ -15,25 +15,19 @@ Files_list = [
     "A2_Kinetic.csv",
     "A3_Kinetic.csv"
 ]
+if Plot_psdA:
+    File_used = Files_list[1]
+    t,x,y = Extract(File_used,"Time"), Extract(File_used,"x"), Extract(File_used,"y")
 
-if Plot_PSD:
-    plt.figure("PSD")
+    plt.figure("xy")
     plt.clf()
-    for file in Files_list:
-        DATA = Csv2Dict(file)
-        t = np.array(DATA["Time"])[1::100]  # Compresse les données par 100 et ne prends pas la 1ere valeur
-        Vx, Vy = np.array(DATA["vx"])[1::100], np.array(DATA["vy"])[1::100]
-        V = np.sqrt(Vx**2 + Vy**2)
-        plt.semilogy(fft.freq(t), fft.psd(V), label=file[:2])  # file[:2] pour ne pas affiché le "_Kinetic.csv" à la fin
-        plt.legend(loc="upper right")
+    plt.plot(t,x,"b-",label=File_used.split("_")[0])
+    plt.plot(t,y,"r-",label=File_used.split("_")[0])
+    plt.legend(loc="upper right")
 
-if Specific_Plot:
-    file = "A1_Kinetic.csv"
-    plt.figure(1)
+    plt.figure("PSD xy")
     plt.clf()
-    DATA = Csv2Dict(file)
-    t = np.array(DATA["Time"])[1::100]  #Compresse les données par 100 et ne prends pas la 1ere valeur
-    Vx, Vy = np.array(DATA["vx"])[1::100], np.array(DATA["vy"])[1::100]
-    V = np.sqrt(Vx**2 + Vy**2)
-    plt.loglog(fft.freq(t), fft.psd(V), label=file[:2])  # file[:2] pour ne pas affiché le "_Kinetic.csv" à la fin
+    t,x,y = Extract(File_used,"Time"), Extract(File_used,"x"), Extract(File_used,"y")
+    plt.plot(fft.freq(t),fft.psd(x,dB=True),"b-",label=File_used.split("_")[0])
+    plt.plot(fft.freq(t),fft.psd(y,dB=True),"r-",label=File_used.split("_")[0])
     plt.legend(loc="upper right")
